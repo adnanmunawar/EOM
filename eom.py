9ee431765a631a122ed1539ecb7bdc66893830d8
Adnan Munawar <amunawar@wpi.edu>
2017-11-23 17:26:12 -0500

    Updated plotting options and data storage

--- eom.py
+++ eom.py
@@ -7,11 +7,18 @@ class Obj():
     def __init__(self):
         self.d0 = 0
         self.v0 = 0
-        self.a = 0
+        self.a0 = 0
+        self.F0 = 0
+
+        self.d = 0
         self.v = 0
+        self.a = 0
+        self.F = 0
+
         self.t = 0
         self.dt = 0.001
-        self.m = 1;
+
+        self.m = 1
 
         self.d_array = []
         self.v_array = []
@@ -19,19 +26,26 @@ class Obj():
         self.f_array = []
         self.t_array = []
 
+        self._m_store_trajectory = False
+
     def eom(self):
         self.v = self.v0 + self.a * self.t
-        d = self.d0 + 0.5 * (self.v0 + self.v) * self.t
-
-        self.v0 = self.v
-        self.d0 = d
-
-        return d
+        self.d = self.d0 + 0.5 * (self.v0 + self.v) * self.t
+        return self.d
 
     def apply_force(self, F):
-        self.a = F / self.m
+        self.F = F
+        self.a = self.F / self.m
 
     def update(self, dt):
+        if self._m_store_trajectory:
+            self.trajectory()
+
+        self.d0 = self.d
+        self.v0 = self.v
+        self.a0 = self.a
+        self.F0 = self.F
+
         self.dt = dt
         self.t = self.t + dt
         d = self.eom()
@@ -40,17 +54,30 @@ class Obj():
     def set_mass(self, m):
         self.m = m
 
-    def store_trajectory(self, d,v,a, F):
-        self.d_array.append(d)
-        self.v_array.append(v)
-        self.a_array.append(a)
-        self.f_array.append(F)
+    def store_trajectory(self, check):
+        self._m_store_trajectory = check
+
+    def trajectory(self):
+        self.d_array.append(self.d0)
+        self.v_array.append(self.v0)
+        self.a_array.append(self.a0)
+        self.f_array.append(self.F0)
         self.t_array.append(self.t)
 
-    def plot_trajectory(self):
-        plt.plot(self.t_array, self.d_array)
+    def plot_trajectory(self, data_type='Position'):
+        if data_type == 'Position':
+            Y = self.d_array
+        if data_type == 'Velocity':
+            Y = self.v_array
+        if data_type == 'Acceleration':
+            Y = self.a_array
+        if data_type == 'Force':
+            Y = self.f_array
+
+        plt.plot(self.t_array, Y)
         plt.xlabel('Time')
-        plt.ylabel('d = Distance')
+        plt.ylabel(data_type)
+        plt.grid('on')
         plt.show()
 
 
@@ -59,15 +86,15 @@ def run():
 
     my_obj = Obj()
     my_obj.apply_force(0.5)
+    my_obj.store_trajectory(True)
     dt = 0.001
     for i in range(1,500):
         d = my_obj.update(dt)
         my_obj.apply_force(0)
-        my_obj.store_trajectory(d, 0, 0, 0)
         print 'Iteration number ', i, ' d =', d
 
     print my_obj.t_array
-    my_obj.plot_trajectory()
+    my_obj.plot_trajectory('Acceleration')
 
 
 if __name__ == '__main__':
